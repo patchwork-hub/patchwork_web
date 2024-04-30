b31d806d7dddd835631b29a653cace2a5dc2c9fa
SithuBo <sithubo.stb97@gmail.com>
2024-04-30 17:12:19 +0630

    formatted

--- app/controllers/server_settings_controller.rb
+++ app/controllers/server_settings_controller.rb
@@ -1,23 +1,25 @@
-class ServerSettingsController < ApplicationController
+# frozen_string_literal: true
 
+class ServerSettingsController < ApplicationController
   def index
     server_settings = Patchwork::ServerSetting.where(parent_id: nil).includes(:children)
-  
+
     # user_settings = current_user ? Patchwork::UserServerSetting.where(user_id: current_user.id) : []
-  
+
     # server_setting_data = server_settings.each_with_object({}) do |setting, data|
     #   category = setting.name
     #   data[category] ||= {}
-  
+
     #   setting.children.each do |child_setting|
     #     data[category][child_setting.name] = child_setting.value
-  
+
     #     user_setting = user_settings.find { |us| us.server_setting.id == child_setting.id }
     #     data[category][child_setting.name] = user_setting&.value if user_setting
     #   end
     # end
+
     server_setting_data = {}
-    
+
     server_settings.each do |server_setting|
       category = server_setting.name
       server_setting_data[category] ||= {}
@@ -34,5 +36,4 @@ class ServerSettingsController < ApplicationController
       end
     end
   end
-  
 end
